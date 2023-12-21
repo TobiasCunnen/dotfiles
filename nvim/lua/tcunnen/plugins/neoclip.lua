71167d9c20d64eed415fab68e61b6a9e0dbb3031
Tobias Cunnen <Tobias.Cunnen@science.ru.nl>
2023-12-21 12:50:35 +0100

    Neoclip now pastes with enter

--- nvim/lua/tcunnen/plugins/neoclip.lua
+++ nvim/lua/tcunnen/plugins/neoclip.lua
@@ -2,7 +2,15 @@ return {
     'AckslD/nvim-neoclip.lua',
     opts = {},
     config = function()
-        require('neoclip').setup();
+        require('neoclip').setup({
+            keys = {
+                telescope = {
+                    n = {
+                        paste = { 'p', '<CR>' },
+                    },
+                },
+            },
+        });
         require('telescope').load_extension('neoclip')
 
         vim.keymap.set('n', '<leader>nc', ':Telescope neoclip initial_mode=normal<cr>', { desc = 'Telescope [N]eo[C]lip' })
