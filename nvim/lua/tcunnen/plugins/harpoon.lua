c74ea9a43477e2bd89096019d764a9e356421f21
Tobias Cunnen <Tobias.Cunnen@science.ru.nl>
2024-01-18 12:26:15 +0100

    zellij config update

--- nvim/lua/tcunnen/plugins/harpoon.lua
+++ nvim/lua/tcunnen/plugins/harpoon.lua
@@ -8,14 +8,14 @@ return {
         local mark = require("harpoon.mark")
         local ui = require("harpoon.ui")
 
-        vim.keymap.set("n", "<A-a>", mark.add_file, { desc = "Set Harpoon Mark" })
-        vim.keymap.set("n", "<A-u>", ui.toggle_quick_menu, { desc = "Show Harpoon Ui" })
+        vim.keymap.set("n", "fa", mark.add_file, { desc = "Set Harpoon Mark" })
+        vim.keymap.set("n", "fu", ui.toggle_quick_menu, { desc = "Show Harpoon Ui" })
 
-        vim.keymap.set("n", "<A-h>", function() ui.nav_file(1) end)
-        vim.keymap.set("n", "<A-j>", function() ui.nav_file(2) end)
-        vim.keymap.set("n", "<A-k>", function() ui.nav_file(3) end)
-        vim.keymap.set("n", "<A-l>", function() ui.nav_file(4) end)
-        vim.keymap.set("n", "<A-;>", function() ui.nav_file(5) end)
-        vim.keymap.set("n", "<A-'>", function() ui.nav_file(6) end)
+        vim.keymap.set("n", "fh", function() ui.nav_file(1) end)
+        vim.keymap.set("n", "fj", function() ui.nav_file(2) end)
+        vim.keymap.set("n", "fk", function() ui.nav_file(3) end)
+        vim.keymap.set("n", "fl", function() ui.nav_file(4) end)
+        vim.keymap.set("n", "f;", function() ui.nav_file(5) end)
+        vim.keymap.set("n", "f'", function() ui.nav_file(6) end)
     end,
 }
