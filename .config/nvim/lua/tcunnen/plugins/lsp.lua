cbb62ab2b3303569899b44bc595b1590fe5a7cef
TobiasCunnen <t.cunnen@gmail.com>
2024-02-28 00:13:31 +0100

    Created rename function that uses treesitter if lsp does not have rename

--- .config/nvim/lua/tcunnen/plugins/lsp.lua
+++ .config/nvim/lua/tcunnen/plugins/lsp.lua
@@ -18,12 +18,50 @@ return {
             require('cmp_nvim_lsp').default_capabilities()
         )
 
+        -- Rename function that only exists because the Perl LSP does not have rename capabilities
+        function Rename()
+
+            local clients = vim.lsp.get_active_clients({ bufnr = vim.api.nvim_get_current_buf() })
+
+            for _, client in pairs(clients) do
+                if client.supports_method("textDocument/rename") then
+                    return vim.lsp.buf.rename()
+                end
+            end
+
+            local node = vim.treesitter.get_node()
+            if node == nil then return end
+            local range = Get_range(node)
+
+            local current_word = vim.fn.expand("<cword>")
+            vim.ui.input({ prompt = "New name: ", default = current_word }, function(new_name)
+                if new_name == nil then return end
+                vim.cmd(':'.. range .. 's/' .. current_word .. '/' .. new_name .. '/g')
+            end)
+
+            vim.cmd('noh')
+        end
+
+        -- This only works for Perl now
+        function Get_range(node)
+            local parent = node:parent()
+            local start_pos, col1, end_pos, col2 = node:range()
+            start_pos = start_pos + 2
+            if parent == nil or parent:type() == 'source_file' then
+                return '%'
+            end
+            if parent:type() == 'subroutine_declaration_statement' or parent:type() == 'conditional_statement' then
+                return start_pos .. ',' .. end_pos
+            end
+            return Get_range(parent)
+        end
+
         vim.api.nvim_create_autocmd('LspAttach', {
             desc = 'LSP actions',
             callback = function(event)
                 local opts = { buffer = event.buf }
 
-                vim.keymap.set('n', 'K',  vim.lsp.buf.hover, opts)
+                vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
                 vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
                 vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
                 vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
@@ -33,7 +71,7 @@ return {
                 vim.keymap.set('n', 'gl', vim.diagnostic.open_float, opts)
                 vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, opts)
                 vim.keymap.set('n', ']d', vim.diagnostic.goto_next, opts)
-                vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, opts)
+                vim.keymap.set('n', '<leader>rn', Rename, opts)
                 vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, opts)
                 vim.keymap.set({ 'n', 'x' }, '<leader>fc', vim.lsp.buf.format, opts)
             end
