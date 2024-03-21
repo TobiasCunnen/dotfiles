216260917fda2cbf7cd858de06e5e11f50c3ceaf
Tobias Cunnen <Tobias.Cunnen@science.ru.nl>
2024-03-21 12:49:31 +0100

    Change look of lualine

--- .config/nvim/lua/tcunnen/plugins/lualine.lua
+++ .config/nvim/lua/tcunnen/plugins/lualine.lua
@@ -4,6 +4,17 @@ return {
     opts = {
         options = {
             theme = 'tokyonight',
+            icons_enabled = true,
+            component_separators = { left = '', right = '' },
+            section_separators = { left = '', right = '' },
+        },
+        sections = {
+            lualine_a = { 'mode' },
+            lualine_b = { 'branch', 'diff', 'diagnostics' },
+            lualine_c = { 'filename' },
+            lualine_x = { 'filetype' },
+            lualine_y = { 'progress' },
+            lualine_z = { 'location' }
         },
     },
 }
