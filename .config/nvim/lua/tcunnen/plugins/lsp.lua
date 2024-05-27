f18196773939782bbb546d47747ef0eeb6709197
Tobias Cunnen <Tobias.Cunnen@science.ru.nl>
2024-05-27 10:56:46 +0200

    Added keybings for trouble plugin

--- .config/nvim/lua/tcunnen/plugins/lsp.lua
+++ .config/nvim/lua/tcunnen/plugins/lsp.lua
@@ -66,7 +66,6 @@ return {
                 vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
                 vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
                 vim.keymap.set('n', 'go', vim.lsp.buf.type_definition, opts)
-                vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
                 vim.keymap.set('n', 'gs', vim.lsp.buf.signature_help, opts)
                 vim.keymap.set('n', 'gl', vim.diagnostic.open_float, opts)
                 vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, opts)
--- .config/nvim/lua/tcunnen/plugins/trouble.lua
+++ .config/nvim/lua/tcunnen/plugins/trouble.lua
@@ -3,4 +3,12 @@ return {
     event = "VeryLazy",
     dependencies = { "nvim-tree/nvim-web-devicons" },
     opts = {},
+    config = function()
+        vim.keymap.set("n", "<leader>xx", function() require("trouble").toggle() end)
+        vim.keymap.set("n", "<leader>xw", function() require("trouble").toggle("workspace_diagnostics") end)
+        vim.keymap.set("n", "<leader>xd", function() require("trouble").toggle("document_diagnostics") end)
+        vim.keymap.set("n", "<leader>xq", function() require("trouble").toggle("quickfix") end)
+        vim.keymap.set("n", "<leader>xl", function() require("trouble").toggle("loclist") end)
+        vim.keymap.set("n", "gr", function() require("trouble").toggle("lsp_references") end)
+    end
 }
