636dbf965df4dfa3e6374db65c3f3db773285f23
Tobias Cunnen <Tobias.Cunnen@science.ru.nl>
2024-06-28 13:59:46 +0200

    Update surround keymaps

--- config/nvim/lua/plugins/nvim-surround.lua
+++ config/nvim/lua/plugins/nvim-surround.lua
@@ -3,6 +3,19 @@ return {
     version = '*', -- Use for stability; omit to use `main` branch for the latest features
     event = 'VeryLazy',
     config = function()
-        require('nvim-surround').setup({})
+        require('nvim-surround').setup({
+            keymaps = {
+                snsert = '<C-g>a',
+                insert_line = '<C-g>A',
+                normal = '<leader>sa',
+                normal_cur = '<leader>sA',
+                normal_line = '<leader>saa',
+                normal_cur_line = '<leader>sAa',
+                visual = '<leader>sa',
+                visual_line = '<leader>sA',
+                delete = '<leader>sd',
+                change = '<leader>sc',
+            },
+        })
     end
 }
